79641a39d38cecd370a29d49b660524e76c58784
ivanjermakov <vania6600@gmail.com>
2019-11-18 13:12:35 +0300

    refactoring

--- kbmap.py
+++ kbmap.py
@@ -1,65 +1,65 @@
 import atexit
+import importlib
+import importlib.util as iu
 
 from evdev import KeyEvent, ecodes, InputDevice, list_devices, UInput
 
 from combination import Combination
-from mapping import Mapping
 
 
-def _transparent(e):
+def _transparent(dev, e):
     dev.write(e.type, e.code, e.value)
 
 
-# TODO
-def _release_all_keys(dev):
-    for k in ecodes.keys:
-        dev.write(ecodes.EV_KEY, k, KeyEvent.key_up)
+def _load_mappings(path):
+    spec = iu.spec_from_file_location("config", path)
+    config = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(config)
+    return config.mappings
 
 
-mappings = [
-    Mapping(Combination(ecodes.KEY_J, [ecodes.KEY_CAPSLOCK]), Combination(ecodes.KEY_LEFT)),
-    Mapping(Combination(ecodes.KEY_K, [ecodes.KEY_CAPSLOCK]), Combination(ecodes.KEY_DOWN)),
-    Mapping(Combination(ecodes.KEY_L, [ecodes.KEY_CAPSLOCK]), Combination(ecodes.KEY_RIGHT)),
-    Mapping(Combination(ecodes.KEY_I, [ecodes.KEY_CAPSLOCK]), Combination(ecodes.KEY_UP)),
-]
+def apply(config_path, device_name, uinput_name='kbmap'):
+    mappings = _load_mappings(config_path)
 
-device_fn = '/dev/input/event7'
-devices = [InputDevice(fn) for fn in list_devices()]
-keyboard = [d for d in devices if device_fn in d.fn][0]
-atexit.register(keyboard.ungrab)
-keyboard.grab()
+    devices = [InputDevice(fn) for fn in list_devices()]
+    keyboard = [d for d in devices if device_name == d.name][0]
+    atexit.register(keyboard.ungrab)
+    keyboard.grab()
 
-dev = UInput.from_device(keyboard, name='kbmap')
-with dev:
-    for e in keyboard.read_loop():
-        # key event
-        if e.type == ecodes.EV_KEY:
-            combination = Combination(
-                e.code,
-                list(filter(lambda k: k != e.code, keyboard.active_keys()))
-            )
-            # print(f'combination: {combination}')
-            matched_mappings = list(
-                filter(lambda m: m.source.matching(combination), mappings)
-            )
-            if e.code in map(lambda m: m.source.key, mappings) and matched_mappings:
-                matched_mapping = matched_mappings[0]
-                # print(f'matched mapping: {matched_mapping}')
-                remapped_code = matched_mapping.target.key
-                # key press
-                if e.value == KeyEvent.key_down:
-                    for m in matched_mapping.target.modifiers:
-                        dev.write(e.type, m, KeyEvent.key_up)
-                    dev.write(e.type, remapped_code, e.value)
-                    for m in matched_mapping.target.modifiers:
-                        dev.write(e.type, m, KeyEvent.key_down)
-                # key release
-                elif e.value == KeyEvent.key_up:
-                    dev.write(e.type, remapped_code, e.value)
-                # key hold
+    dev = UInput.from_device(keyboard, name=uinput_name)
+    with dev:
+        for e in keyboard.read_loop():
+            # key event
+            if e.type == ecodes.EV_KEY:
+                combination = Combination(
+                    e.code,
+                    list(filter(lambda k: k != e.code, keyboard.active_keys()))
+                )
+                # print(f'combination: {combination}')
+                matched_mappings = list(
+                    filter(lambda m: m.source.matching(combination), mappings)
+                )
+                if e.code in map(lambda m: m.source.key, mappings) and matched_mappings:
+                    matched_mapping = matched_mappings[0]
+                    # print(f'matched mapping: {matched_mapping}')
+                    remapped_code = matched_mapping.target.key
+                    # key press
+                    if e.value == KeyEvent.key_down:
+                        for m in matched_mapping.target.modifiers:
+                            dev.write(e.type, m, KeyEvent.key_up)
+                        dev.write(e.type, remapped_code, e.value)
+                        for m in matched_mapping.target.modifiers:
+                            dev.write(e.type, m, KeyEvent.key_down)
+                    # key release
+                    elif e.value == KeyEvent.key_up:
+                        dev.write(e.type, remapped_code, e.value)
+                    # key hold
+                    else:
+                        _transparent(dev, e)
                 else:
-                    _transparent(e)
+                    _transparent(dev, e)
             else:
-                _transparent(e)
-        else:
-            _transparent(e)
+                _transparent(dev, e)
+
+
+apply("config.py", 'SINO WEALTH USB KEYBOARD')
