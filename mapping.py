13a1f7ca4db4ae6bdf070972db4fb0b40a059b76
ivanjermakov <vania6600@gmail.com>
2019-11-18 13:11:35 +0300

    docs

--- mapping.py
+++ mapping.py
@@ -2,10 +2,10 @@ from enum import Enum
 
 
 class MappingType(Enum):
-    """when source mapping appear in combination with not included modifiers: copy them to target combination"""
     copyModifiers = 1
-    """when source mapping appear in combination with not included modifiers: do not match source combination"""
+    """when source mapping appear in combination with not included modifiers - copy them to target combination"""
     matchModifiers = 2
+    """when source mapping appear in combination with not included modifiers - do not match source combination"""
 
 
 class Mapping:
