952fcde83e0d5e635f1a422a2ada49883224b4fa
ivanjermakov <ivanjermakov1@gmail.com>
2020-08-27 11:58:30 +0300

    #13 api improvement

--- kbmap.py
+++ kbmap.py
@@ -9,7 +9,7 @@ import mapper
 @click.command(short_help='Perform mapping.')
 @click.argument('config_path')
 @click.argument('device_name')
-@click.option('--uinput-name', default='kbmap', help='Name of the virtual device that will write events.')
+@click.option('--name', '-n', default='kbmap', help='Name of the virtual device that will write events.')
 @click.option('--verbose', '-v', is_flag=True, help='Print detailed logs')
 def kbmap(config_path, device_name, uinput_name, verbose):
     """
