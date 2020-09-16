7da24bf5b7324acbe806cda4d62057d2158f07e4
ivanjermakov <ivanjermakov1@gmail.com>
2020-09-16 18:38:15 +0300

    #42 config validation

--- kbmap/config.py
+++ kbmap/config.py
@@ -10,6 +10,23 @@ from typing import *
 from kbmap.log import *
 
 
+class ErrorList:
+    """
+    List of errors
+    """
+
+    errors: List[str]
+
+    def __init__(self, errors: List[str]) -> None:
+        self.errors = errors
+
+    def empty(self) -> bool:
+        """
+        Check whether error list is empty.
+        """
+        return len(self.errors) == 0
+
+
 class Config:
     """
     Config class.
@@ -24,7 +41,7 @@ class Config:
                  keymaps: List[List[int]],
                  tapping_term: int,
                  kbmap_default_enabled: bool
-                 ):
+                 ) -> None:
         self.physical_layout = physical_layout
         self.keymaps = keymaps
         self.tapping_term = tapping_term
@@ -43,14 +60,14 @@ def from_module(module: Any) -> Config:
     Construct config from config file.
     """
     return Config(
-        module.physical_layout,
-        module.keymaps,
-        module.tapping_term,
-        module.kbmap_default_enabled,
+        module.physical_layout if hasattr(module, 'physical_layout') else None,
+        module.keymaps if hasattr(module, 'keymaps') else None,
+        module.tapping_term if hasattr(module, 'tapping_term') else None,
+        module.kbmap_default_enabled if hasattr(module, 'kbmap_default_enabled') else None
     )
 
 
-def load_config(path=None) -> Config:
+def load(path=None) -> Config:
     """
     Load config from specified path.
     If path is not specified, use DEFAULT_CONFIG_PATH instead.
@@ -62,3 +79,24 @@ def load_config(path=None) -> Config:
     spec.loader.exec_module(config)
     debug(f'config loaded')
     return from_module(config)
+
+
+def validate(config: Config) -> ErrorList:
+    """
+    Validate config for errors
+    """
+    errors = []
+
+    if not config.physical_layout:
+        errors.append('no physical layout')
+
+    if not config.keymaps:
+        errors.append('no keymaps')
+
+    if not config.tapping_term:
+        errors.append('no tapping term')
+
+    if not config.kbmap_default_enabled:
+        errors.append('no kbmap_default_enabled')
+
+    return ErrorList(errors)
